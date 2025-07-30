a59da1801566443bd1cfae685682194e4018414c
wien <q.fumbled@proton.me>
2025-07-30 14:20:16 -0400

    Nix is trying to import ./modules/nixvim/colors/paradise.nix, but it doesn’t exist in the Git-tracked source, and so it isn’t included in the Nix store. thats really annoying

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,4 +1,10 @@
 {
-  # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+  imports = [ ./bufferline.nix ./colors/paradise.nix ];
+
+  config = {
+    theme = "paradise";
+    extraConfigLua = ''
+      _G.theme = "paradise"
+    '';
+  };
 }
